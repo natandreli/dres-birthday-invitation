3fc487f4dfaac1b9ca1afacd1401e8c7f7a359d8
Natalia Andrea García Ríos <110438161+natandreli@users.noreply.github.com>
2023-10-08 19:08:52 -0500

    Eliminé el componente Background, no sirvió para la optimización y bajó la calidad del fondo

--- app/invitation/page.jsx
+++ app/invitation/page.jsx
@@ -2,7 +2,6 @@
 
 import React, { useState, useEffect } from "react";
 import Confetti from "react-confetti";
-import Background from "@/components/Background";
 
 export default function Invitation() {
   const [backgroundColor, setBackgroundColor] = useState(getRandomColor());
@@ -29,52 +28,49 @@ export default function Invitation() {
   }
 
   return (
-    <Background src="/images/capycollage2.webp" alt="Capybara collage">
+    <div
+      id="invitation-page"
+      className="min-h-screen flex items-center justify-center"
+    >
+      {confetti && (
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          numberOfPieces={300}
+        />
+      )}
       <div
-        id="invitation-page"
-        className="min-h-screen flex items-center justify-center"
+        id="invitation-card-container"
+        className="w-96 h-120 bg-blue-200 p-6 rounded-lg"
+        style={{ backgroundColor }}
       >
-        {confetti && (
-          <Confetti
-            width={window.innerWidth}
-            height={window.innerHeight}
-            numberOfPieces={300}
-          />
-        )}
-        <div
-          id="invitation-card-container"
-          className="w-96 h-120 bg-blue-200 p-6 rounded-lg"
-          style={{ backgroundColor }}
-        >
-          <div className="bg-card p-4 rounded-lg shadow-md text-black">
-            <h1 className="text-4xl font-semibold mb-4 text-center">
-              ¡Sabía que no podrías negarte!
-            </h1>
-            <p className="text-lg text-center">
-              <strong>Lugar:</strong> Cervecería Lardi Brewing, Bello Cl 37
-              #52-37
-            </p>
-            <br />
-            <p className="text-lg text-center">
-              <strong>Día:</strong> Viernes, 13 de octubre
-            </p>
-            <br />
-            <p className="text-lg text-center">
-              <strong>Hora:</strong> 8:00 pm (?)
-            </p>
-            <br />
-            <p className="text-lg text-center">
-              ¡No olvides mi regalo! (Es broma, pero si quieres no es broma)
-            </p>
-            <p className="text-lg text-center mt-4 text-sm">
-              <em>
-                *Al presionar <strong>Sí, me encantaría</strong> has vendido tu
-                alma a este plan
-              </em>
-            </p>
-          </div>
+        <div className="bg-card p-4 rounded-lg shadow-md text-black">
+          <h1 className="text-4xl font-semibold mb-4 text-center">
+            ¡Sabía que no podrías negarte!
+          </h1>
+          <p className="text-lg text-center">
+            <strong>Lugar:</strong> Cervecería Lardi Brewing, Bello Cl 37 #52-37
+          </p>
+          <br />
+          <p className="text-lg text-center">
+            <strong>Día:</strong> Viernes, 13 de octubre
+          </p>
+          <br />
+          <p className="text-lg text-center">
+            <strong>Hora:</strong> 8:00 pm (?)
+          </p>
+          <br />
+          <p className="text-lg text-center">
+            ¡No olvides mi regalo! (Es broma, pero si quieres no es broma)
+          </p>
+          <p className="text-lg text-center mt-4 text-sm">
+            <em>
+              *Al presionar <strong>Sí, me encantaría</strong> has vendido tu
+              alma a este plan
+            </em>
+          </p>
         </div>
       </div>
-    </Background>
+    </div>
   );
 }
